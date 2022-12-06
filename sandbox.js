c21be06237fbf6e2c0996ab743471aedf0161fa2
rensilva5 <renzoasilva@gmail.com>
2022-12-05 23:23:42 -0500

    objects and arrays conditions

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,25 @@
-var character = 'Renzo';
-var age = 30;
-var isGreen = false;
-character = "Silva";
+var names = ['carl', 'tommy', 'tom'];
+names.push('charles');
+// names.push(4)  the type cannot be changed 
+// names[0] = 3   the type cannot be reasigned with a different type.
+var numbers = [10, 20, 40, 50];
+// when we have declared different types, we can assign, reassign, or change. See example bellow 
+var mixed = ['jhon', 4, true, 'thomas'];
+mixed.push(5);
+mixed.push('martin');
+mixed[0] = 5;
+console.log(mixed);
+// Objects //type cannot be changes as in variables.
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40; //this is fine
+ninja.name = 'tarzicio'; //this is also fine.
+// ninja.name = 35 this is not gonna work.
 // In typeSvcript a type will always be the same
 // variables can chance, but with the same type.
 // Double and single quatiotion are ok.
 // JS uses Inference.
 // Less errors in the process.
-
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(6));
--- sandbox.ts
+++ sandbox.ts
@@ -1,19 +1,38 @@
-let character = 'Renzo'
+let names = ['carl', 'tommy', 'tom']
 
-let age = 30
+names.push('charles')
 
-let isGreen = false
+// names.push(4)  the type cannot be changed 
+// names[0] = 3   the type cannot be reasigned with a different type.
 
-character = "Silva"
+let numbers = [10, 20, 40, 50]
+
+
+// when we have declared different types, we can assign, reassign, or change. See example bellow 
+let mixed = ['jhon', 4, true, 'thomas'] 
+
+mixed.push(5)
+mixed.push('martin')
+mixed[0] = 5
+
+console.log(mixed)
+
+// Objects //type cannot be changes as in variables.
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+}
+
+ninja.age = 40 //this is fine
+ninja.name = 'tarzicio' //this is also fine.
+// ninja.name = 35 this is not gonna work.
+
+
+
 
 // In typeSvcript a type will always be the same
 // variables can chance, but with the same type.
 // Double and single quatiotion are ok.
 // JS uses Inference.
 // Less errors in the process.
-
-const circ = (diameter: number) => {
-    return diameter * Math.PI
-}
-
-console.log(circ(6))
