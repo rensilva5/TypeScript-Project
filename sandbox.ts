31deb5720f8dea02725bb097ecf9ea594d33f98b
rensilva5 <renzoasilva@gmail.com>
2022-12-05 20:12:41 -0500

    Compile typescript into javaScript

---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const character = 'Renzo'
+
+console.log(character)
+
+const inputs = document.querySelectorAll("input")
+
+console.log(inputs)
+
+inputs.forEach(function (input) {
+    console.log(input)
+})
