a908772002be9deff5d21a0c0fad7cf4ee94b5dc
rensilva5 <renzoasilva@gmail.com>
2022-12-08 22:37:31 -0500

    Using classes

--- public/app.js
+++ public/app.js
@@ -1,13 +1,34 @@
 "use strict";
 // const anchor = document.querySelector('a')!;
 // if (anchor) {
-//     console.log(anchor.href)
+//    console.log(anchor.href)
 // }
 // or
 // console.log(anchor.href)
 // const form = document.querySelector('form')!;
 // console.log(form)
 // const form = document.querySelector('.new-item-form')! 
+// Classes --> blueprint for an object
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} ${this.details}`;
+    }
+}
+const invOne = new Invoice('Harry', 'work from home', 230);
+const invTwo = new Invoice('Larry', 'work from office', 270);
+// console.log(invOne, invTwo)
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = 'Paul';
+invTwo.amount = 300;
+console.log(invOne, invTwo);
+console.log(invoices);
 const form = document.querySelector('.new-item-form'); // the we can access diffrent methods.
 // console.log(form.children)
 // inputs
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,50 @@
 // const anchor = document.querySelector('a')!;
 
 // if (anchor) {
-    //     console.log(anchor.href)
-    // }
+//    console.log(anchor.href)
+// }
     
-    // or
-    // console.log(anchor.href)
-    // const form = document.querySelector('form')!;
-    // console.log(form)
+// or
+// console.log(anchor.href)
+// const form = document.querySelector('form')!;
+// console.log(form)
 
-    // const form = document.querySelector('.new-item-form')! 
+// const form = document.querySelector('.new-item-form')! 
+
+// Classes --> blueprint for an object
+
+class Invoice {
+    client: string;
+    details: string;
+    amount: number
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c
+        this.details = d
+        this.amount = a
+    }
+    format() {
+        return `${this.client} owes $${this.amount} ${this.details}`
+    }
+}
+
+const invOne = new Invoice('Harry', 'work from home', 230)
+const invTwo = new Invoice('Larry', 'work from office', 270)
+
+// console.log(invOne, invTwo)
+let invoices: Invoice[] = []
+invoices.push(invOne)
+invoices.push(invTwo)
+
+invOne.client = 'Paul'
+invTwo.amount = 300
+
+console.log(invOne, invTwo)
+
+console.log(invoices)
+
+
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement // the we can access diffrent methods.
 // console.log(form.children)
 
